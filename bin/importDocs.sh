6ddad1256a4da8eeebbdd4ee0a5cf3410184def0
Jeffrey Alexander <jeffrey.alexander@oracle.com>
2020-08-25 11:03:17 -0400

    Updated FAQ import with correct URL for graphic

--- bin/importDocs.sh
+++ bin/importDocs.sh
@@ -83,7 +83,7 @@ head -1 $TRIBUO_DOC_DIR/FAQs.md >> $SITE_ROOT/learn/$DOC_VERSION/faq.md
 cat << EOF >> $SITE_ROOT/learn/$DOC_VERSION/faq.md
 {:.no_toc}
 
-![faq graphic](http://localhost:4000/assets/img/faq.svg)
+![faq graphic](https://tribuo.org/assets/img/faq.svg)
 
 * TOC
 {:toc}
